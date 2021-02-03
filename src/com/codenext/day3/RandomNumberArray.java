89908599077370f07eca966efd874e2dda9d979b
golima <lima01@gmail.com>
2021-02-03 18:40:15 -0500

    Print array in revert order.
    Order array.

--- src/com/codenext/day3/RandomNumberArray.java
+++ src/com/codenext/day3/RandomNumberArray.java
@@ -1,6 +1,7 @@
 package com.codenext.day3;
 
 import com.codenext.util.ArrayUtils;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -11,10 +12,28 @@ public class RandomNumberArray {
 
     int[] arr = new int[size];
     for( int i=0; i<arr.length; i++){
-      arr[i] = new Random().nextInt();
+      arr[i] = new Random().nextInt(size);
     }
 
     System.out.println("Array has been initialized as below:");
+    for (int i=0; i<arr.length; i++) {
+      if ( i!=0 ) {//skip the first item, so we won't get , in front of it.
+        System.out.print(", ");
+      }
+      System.out.print(String.valueOf(arr[i]));
+    }
+
+    System.out.println();
+
+    for (int i=arr.length-1; i>=0; i--) {
+      if ( i!=arr.length-1 ) {//skip the first item, so we won't get , in front of it.
+        System.out.print(", ");
+      }
+      System.out.print(String.valueOf(arr[i]));
+    }
+
+    Arrays.sort(arr);
+    System.out.println();
     ArrayUtils.printArray(arr);
   }
 }
