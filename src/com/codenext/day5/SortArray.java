6bacc679f2b4962d0e437ff281ee92c6c680c96f
golima <lima01@gmail.com>
2021-02-12 16:11:21 -0500

    Reformat SortArray

--- src/com/codenext/day5/SortArray.java
+++ src/com/codenext/day5/SortArray.java
@@ -8,6 +8,7 @@ import static com.codenext.util.Utils.randomArray;
 import java.util.Scanner;
 
 public class SortArray {
+
   public static void main(String args[]) {
     print("Please enter array size:");
     int size = new Scanner(System.in).nextInt();
@@ -18,12 +19,12 @@ public class SortArray {
 
     int[] arr = randomArray(size);
 
-    for(int i=0; i<arr.length-1; i++) {
+    for (int i = 0; i < arr.length - 1; i++) {
 
       //Find the smallest number from i to end of the array
-      int indexMin=i;
-      for(int j=i+1; j<arr.length; j++){
-        if (arr[j] < arr[indexMin]){
+      int indexMin = i;
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[indexMin]) {
           indexMin = j;
         }
       }
